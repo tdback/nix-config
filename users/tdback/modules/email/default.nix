21a0a5d5a485c72607124908eac63010dd5b7e2d
tdback <tyler@tdback.net>
2025-02-05 22:30:29 -0500

    email: remove neomutt, use mu4e instead

--- users/tdback/modules/email/default.nix
+++ users/tdback/modules/email/default.nix
@@ -8,7 +8,6 @@ let
   username = config.home.username;
   email = "tyler@tdback.net";
   provider = "fastmail.com";
-  mailboxes = lib.attrValues config.accounts.email.accounts.${username}.folders;
 in
 {
   accounts.email.maildirBasePath = "Mail";
@@ -41,83 +40,16 @@ in
         enable = true;
         extraConfig.logfile = "~/.cache/msmtp/msmtp.log";
       };
-
-      neomutt = {
-        enable = true;
-        sendMailCommand = lib.getExe pkgs.msmtp;
-        extraMailboxes = mailboxes;
-      };
     };
   };
 
-  services.mbsync.enable = true;
+  programs.mbsync.enable = true;
+  programs.msmtp.enable = true;
 
-  programs = {
-    mbsync.enable = true;
-    msmtp.enable = true;
-    neomutt =
-      let
-        mkAction =
-          {
-            key,
-            action,
-            map ? [
-              "index"
-              "pager"
-            ],
-          }:
-          {
-            inherit key action map;
-          };
-      in
-      {
-        enable = true;
-        package = pkgs.unstable.neomutt;
-        vimKeys = true;
-        sort = "reverse-date";
-        checkStatsInterval = 60;
-        sidebar.enable = true;
-        binds = [
-          (mkAction {
-            key = "\\Cp";
-            action = "sidebar-prev";
-          })
-          (mkAction {
-            key = "\\Cn";
-            action = "sidebar-next";
-          })
-          (mkAction {
-            key = "\\Cy";
-            action = "sidebar-open";
-          })
-        ];
-        macros = [
-          (mkAction {
-            key = "gi";
-            action = "<change-folder>=Inbox<enter>";
-          })
-          (mkAction {
-            key = "gs";
-            action = "<change-folder>=Sent<enter>";
-          })
-          (mkAction {
-            key = "gd";
-            action = "<change-folder>=Drafts<enter>";
-          })
-          (mkAction {
-            key = "gt";
-            action = "<change-folder>=Trash<enter>";
-          })
-          (mkAction {
-            key = "ga";
-            action = "<change-folder>=Archive<enter>";
-          })
-          (mkAction {
-            map = [ "index" ];
-            key = "S";
-            action = "<shell-escape>${lib.getExe pkgs.isync} -a<enter>";
-          })
-        ];
-      };
-  };
+  home.packages =
+    with pkgs.unstable;
+    lib.mkIf config.programs.emacs.enable [
+    mu
+    mu.mu4e
+  ];
 }
